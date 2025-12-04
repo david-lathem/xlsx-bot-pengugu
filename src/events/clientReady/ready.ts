f5da45c0a7082baf2e3d4dfd0f8342b97e3e3e13
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-04 12:44:34 +0500

    remove the cod einjection

--- src/events/clientReady/ready.ts
+++ src/events/clientReady/ready.ts
@@ -1,10 +1,8 @@
 import { Client } from "discord.js";
 
 import registerAndAttachCommandsOnClient from "../../utils/registrars/registerCommands.js";
-import { removeNumberedCodes } from "../../database/queries.js";
 
 export default async (client: Client<true>) => {
   console.log(`${client.user.username} (${client.user.id}) is ready 🐬`);
-  removeNumberedCodes.run();
   await registerAndAttachCommandsOnClient(client);
 };
